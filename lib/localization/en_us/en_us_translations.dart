8206e2611dac8561e8bd2b1271cab479ff3a2030
oi-taa <124193779+oi-taa@users.noreply.github.com>
2023-09-20 10:06:38 +0530

    Add files via upload

---
+++ lib/localization/en_us/en_us_translations.dart
@@ -0,0 +1,255 @@
+final Map<String, String> enUs = {
+  // Login Screen
+  "lbl_register": "Register",
+  "lbl_sign_in": "Sign in",
+  "msg_don_t_have_an_account": "Don’t have an account? Register",
+  "msg_don_t_have_an_account2": "Don’t have an account?",
+  "msg_forgot_password": "Forgot Password?",
+  "msg_sign_in_to_continue": "Sign in to continue",
+  "msg_welcome_to_hidoc": "Welcome to DocNow",
+
+  // Signup Screen
+  "lbl": "?",
+  "lbl_account": "account",
+  "lbl_full_name": "Full Name",
+  "lbl_have_an": "Have an ",
+  "lbl_password_again": "Password Again",
+  "lbl_sign_in2": "Sign In",
+  "msg_create_an_new_account": "Create an new account",
+  "msg_have_an_account": "Have an account? Sign In",
+  "msg_let_s_get_started": "Let’s Get Started",
+
+  // Dashboard Screen
+  "lbl_1_5km_away": "1,5km away",
+  "lbl_2km_away": "2km away",
+  "lbl_4_8": "4,8",
+  "lbl_4_9": "4,9",
+  "lbl_dr_stevi_jessi": "Dr. Naman Kanwar",
+  "lbl_healt_article": "Healt article",
+  "lbl_learn_more": "Learn more",
+  "msg_dr_marcus_horizo": "Dr. Neha Yadav",
+  "msg_early_protection": "Early protection for\nyour family healt",
+  "msg_find_your_desire": "Find your desire \nhealt solution",
+  "msg_search_doctor_drugs": "Search doctor, drugs, articles...",
+
+  // Dashboard - Container Screen
+  "lbl_appointment": "Appointment", "lbl_home": "Home",
+  "lbl_messages": "Messages", "lbl_profile": "Profile",
+
+  // Dr List Screen
+  "lbl_dr_gerty_cori": "Dr. Gerty Cori",
+  "lbl_dr_stefi_jessi": "Dr. Vasu Sharma",
+
+  // Dr Details Screen
+  "lbl_01_00_pm": "01:00 PM",
+  "lbl_02_00_pm": "02:00 PM",
+  "lbl_03_00_pm": "03:00 PM",
+  "lbl_04_00_pm": "04:00 PM",
+  "lbl_07_00_pm": "07:00 PM",
+  "lbl_08_00_pm": "08:00 PM",
+  "lbl_09_00_am": "09:00 AM",
+  "lbl_10_00_am": "10:00 AM",
+  "lbl_11_00_am": "11:00 AM",
+  "lbl_21": "21",
+  "lbl_22": "22",
+  "lbl_23": "23",
+  "lbl_24": "24",
+  "lbl_25": "25",
+  "lbl_26": "26",
+  "lbl_about": "About",
+  "lbl_book_apointment": "Book Apointment",
+  "lbl_doctor_detail": "Doctor detail",
+  "lbl_fri": "Fri",
+  "lbl_mon": "Mon",
+  "lbl_sat": "Sat",
+  "lbl_thu": "Thu",
+  "lbl_tue": "Tue",
+  "lbl_wed": "Wed",
+  "msg_lorem_ipsum_dolor":
+      "Cardiologist, MAX super speciality, A-13, Dharmashala, Himachal Pradesh",
+
+  // Book an appointment Screen
+  "lbl2": "-",
+  "lbl_01_00": "75 rupees",
+  "lbl_60_00": "4200 rupees",
+  "lbl_61_00": "4270 ruoees",
+  "lbl_61_002": "4270 ruoees",
+  "lbl_admin_fee": "Admin Fee",
+  "lbl_apointment": "Apointment",
+  "lbl_booking": "Booking",
+  "lbl_chest_pain": "Chest pain",
+  "lbl_consultation": "Consultation",
+  "lbl_date": "Date",
+  "lbl_reason": "Reason",
+  "msg_aditional_discount": "Aditional Discount",
+  "msg_wednesday_jun_23": "Wednesday, Jun 23, 2021 | 10:00 AM",
+
+  // Chat Screen
+  "lbl_10_min_ago": "10 min ago",
+  "lbl_5_min_ago": "5 min ago",
+  "lbl_online": "Online",
+  "lbl_send": "Send",
+  "msg_consultion_start": "Consultion Start",
+  "msg_i_don_t_have_any": "I don,t have any fever, \nbut headchace is painful",
+  "msg_i_have_suffering":
+      "I have suffering from headache \nand cold for 3 days, I took 2 \ntablets of dolo, but still pain",
+  "msg_ok_do_you_have": "Ok, Do you have fever? is the\nheadchace severe",
+  "msg_type_message": "Type message ...",
+  "msg_you_can_consult": "You can consult your problem to the doctor",
+
+  // Settigns Screen
+  "lbl_103lbs": "103lbs",
+  "lbl_215bpm": "215bpm",
+  "lbl_756cal": "756cal",
+  "lbl_amelia_renata": "Amelia Renata",
+  "lbl_appointmnet": "Appointmnet",
+  "lbl_calories": "Calories",
+  "lbl_faqs": "FAQs",
+  "lbl_heart_rate": "Heart rate",
+  "lbl_help": "Help",
+  "lbl_my_saved": "My Saved",
+  "lbl_weight": "Weight",
+
+  // Pharmacy Screen
+  "lbl_100ml": "100ml",
+  "lbl_15_99": "1050 rupees",
+  "lbl_3pcs": "3pcs",
+  "lbl_50ml": "50ml",
+  "lbl_5_99": "420 rupees",
+  "lbl_6_99": "490 rupees",
+  "lbl_7_99": "560 rupees",
+  "lbl_betadine": "Betadine",
+  "lbl_bodrex_herbal": "Bodrex Herbal",
+  "lbl_bodrexin": "Bodrexin",
+  "lbl_konidin": "Konidin",
+  "lbl_popular_product": "Popular Product",
+  "lbl_product_on_sale": "Product on Sale",
+  "msg_order_quickly_w": "Order quickly with\nPrescription",
+  "msg_search_drugs_category": "Search drugs, category...",
+  "msg_upload_prescription": "Upload Prescription",
+
+  // Drug Details Screen
+  "lbl_4_0": "4.0",
+  "lbl_buy_now": "Buy Now",
+  "lbl_description": "Description",
+  "lbl_drugs_detail": "Drugs detail",
+  "msg_obh_combi_is_a":
+      "OBH COMBI  is a cough medicine containing, Paracetamol, Ephedrine HCl, and Chlorphenamine maleate which is used to relieve coughs accompanied by flu symptoms such as fever, headache, and sneezing... ",
+
+  // Article Screen
+  "lbl_5_min_read2": "5 min read",
+  "lbl_6_min_read": "6 min read",
+  "lbl_8_min_read": "8 min read",
+  "lbl_articles": "Articles",
+  "lbl_covid_19": "Covid-19",
+  "lbl_diet": "Diet",
+  "lbl_fitness": "Fitness",
+  "lbl_jun_10_20212": "Jun 10, 2021",
+  "lbl_jun_12_2021": "Jun 12, 2021",
+  "lbl_jun_9_2021": "Jun 9, 2021 ",
+  "msg_comparing_the_astrazeneca":
+      "Comparing the \nAstraZeneca and \nSinovac COVID-19 \nVaccines",
+  "msg_popular_articles": "Popular Articles",
+  "msg_related_articles": "Related Articles",
+  "msg_search_articles": "Search articles, news...",
+  "msg_the_horror_of_the":
+      "The Horror Of The \nSecond Wave Of \nCOVID-19 \npandemic",
+  "msg_traditional_herbal":
+      "Traditional Herbal Medicine Treatments for COVID-19",
+  "msg_trending_articles": "Trending Articles",
+
+  // Cart Screen
+  "lbl_19_98": "1400",
+  "lbl_1_00": "70 rupees",
+  "lbl_20_98": "1400 rupees",
+  "lbl_20_982": "1470 rupees",
+  "lbl_checkout": "Checkout",
+  "lbl_my_cart": "My Cart",
+  "lbl_subtotal": "Subtotal",
+  "lbl_taxes": "Taxes",
+
+  // ambulance Screen
+  "lbl_ambulance": "Ambulance",
+  "msg_2640_cabin_creek": "G-52, 3rd floor , Dharmashala, Himachal Pradesh",
+  "msg_confirm_location": "Confirm Location",
+  "msg_search_location": "Search location, ZIP code..",
+
+  // Schedule Screen
+  "lbl_10_30_am": "10:30 AM",
+  "lbl_26_06_2021": "26/06/2021",
+  "lbl_28_06_2021": "28/06/2021",
+  "lbl_2_00_pm": "2:00 PM",
+  "lbl_cancel": "Cancel",
+  "lbl_confirmed": "Confirmed",
+  "lbl_psikeater": "Psikeater",
+  "lbl_reschedule": "Reschedule",
+
+  // Schedule - Tab Container Screen
+  "lbl_canceled": "Canceled", "lbl_completed": "Completed",
+  "lbl_schedule": "Schedule", "lbl_upcoming": "Upcoming",
+
+  // Message Screen
+  "lbl_08_57": "08:57",
+  "lbl_09_04": "09:04",
+  "lbl_10_24": "10.24",
+  "msg_do_you_have_fever": "Do you have fever?",
+  "msg_i_don_t_have_any2": "I don,t have any fever, but headchace...",
+
+  // Message - Tab Container Screen
+  "lbl_all": "All", "lbl_group": "Group", "lbl_message": "Message",
+  "lbl_private": "Private",
+
+  // Common String
+  "lbl_1": "1",
+  "lbl_20pcs": "20pcs",
+  "lbl_4_7": "4,7",
+  "lbl_5min_read": "5min read",
+  "lbl_75ml": "75ml",
+  "lbl_800m_away": "800m away",
+  "lbl_9_99": "800 rupees",
+  "lbl_change": "Change",
+  "lbl_chardiologist": "Cardiologist",
+  "lbl_dr_alysa_hana": "Dr. Manas Sah",
+  "lbl_dr_maria_elena": "Dr. Maria Elena",
+  "lbl_jun_10_2021": "Jun 10, 2021 ",
+  "lbl_login": "Login",
+  "lbl_obh_combi": "OBH Combi",
+  "lbl_orthopedist": "Orthopedist",
+  "lbl_panadol": "Panadol",
+  "lbl_password": "Password",
+  "lbl_payment_detail": "Payment Detail",
+  "lbl_payment_method": "Payment Method",
+  "lbl_pharmacy": "Pharmacy",
+  "lbl_psychologist": "Psychologist",
+  "lbl_read_more": "Read more",
+  "lbl_see_all": "See all",
+  "lbl_sign_up": "Sign up",
+  "lbl_top_doctor": "Top Doctor",
+  "lbl_total": "Total",
+  "lbl_visa": "VISA",
+  "lbl_your_email": "Your Email",
+  "msg_dr_marcus_horizon": "Dr. Ishita Srivastava",
+  "msg_hello_how_can_i": "Hello, How can i help you?",
+  "msg_the_25_healthiest":
+      "The 25 Healthiest Fruits You Can Eat, According to a Nutritionist",
+
+  // App navigation Screen
+  "lbl_ambulance2": "ambulance",
+  "lbl_app_navigation": "App Navigation",
+  "lbl_article": "Article",
+  "lbl_cart": "Cart",
+  "lbl_chat": "Chat",
+  "lbl_dr_details": "Dr Details",
+  "lbl_dr_list": "Dr List",
+  "lbl_drug_details": "Drug Details",
+  "lbl_signup2": "Signup",
+  "lbl_splash_screen": "Splash Screen",
+  "msg_book_an_appointment": "Book an appointment",
+  "msg_check_your_app_s":
+      "Check your app's UI from the below demo screens of your app.",
+  "msg_dashboard_container": "Dashboard - Container",
+
+// Network Error String
+  "msg_network_err": "Network Error",
+  "msg_something_went_wrong": "Something Went Wrong!",
+};
